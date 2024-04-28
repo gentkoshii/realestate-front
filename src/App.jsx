97c31cee5d5f660f9ff816a76146fce221667f0a
gentkoshi <155828361+gentkoshi@users.noreply.github.com>
2024-04-28 19:17:09 +0200

    App update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./Components/navbar";
+import HomeSearch from "./HomePage/homeSearch";
 import HomeProducts from "./HomePage/homeProducts";
 import HomeHelp from "./HomePage/homeHelp";
 import HomeFooter from "./Components/homeFooter";
+import SinglePage from "./SinglePost/singlePost";
 import "./layout.scss";
+import NewPost from "./NewPost/newPost";
 import { useState } from "react";
 //import Card from "./Components/card";
 
